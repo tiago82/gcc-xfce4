b005ece278793a45e7003d5f4287c77acd8f5f2b
Tiago <tiago8204@gmail.com>
2022-11-01 17:57:12 -0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,7 +1,14 @@
 sudo apt update 
-sudo apt upgrade
+sudo apt -y upgrade
 sudo apt install -y xfce4 
 sudo apt upgrade -y tigervnc-standalone-server
+sudo apt upgrade -y wget
+
+
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb
+
+
+
 
 
 
