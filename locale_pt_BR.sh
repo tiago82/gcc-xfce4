ae23485ca0a7abfe8bb61645b0f26cc6b5f05439
Tiago <tiago8204@gmail.com>
2023-01-16 21:52:51 -0300

    Update locale_pt_BR.sh

--- locale_pt_BR.sh
+++ locale_pt_BR.sh
@@ -8,3 +8,12 @@ sudo update-locale LC_ALL=pt_BR.UTF-8 LANG=pt_BR.UTF-8
 
 #
 #https://stackoverflow.com/questions/39760663/docker-ubuntu-bin-sh-1-locale-gen-not-found
+
+
+echo "LC_ALL=pt_BR.UTF-8" | sudo tee -a /etc/environment
+echo "pt_BR.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+echo "LANG=pt_BR.UTF-8" | sudo tee -a /etc/locale.conf
+sudo locale-gen pt_BR.UTF-8
+
+
+#https://askubuntu.com/questions/599808/cannot-set-lc-ctype-to-default-locale-no-such-file-or-directory
