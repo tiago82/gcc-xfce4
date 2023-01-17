3206592645f8d82a00f41dc1bd0db351e176aeb9
Tiago <tiago8204@gmail.com>
2023-01-16 21:58:26 -0300

    Update locale_pt_BR.sh

--- locale_pt_BR.sh
+++ locale_pt_BR.sh
@@ -2,9 +2,9 @@
 
 
 
-sudo apt-get update && sudo apt-get install -y locales
-sudo localedef -i pt_BR -c -f UTF-8 -A /usr/share/locale/locale.alias pt_BR.UTF-8
-sudo update-locale LC_ALL=pt_BR.UTF-8 LANG=pt_BR.UTF-8
+#sudo apt-get update && sudo apt-get install -y locales
+#sudo localedef -i pt_BR -c -f UTF-8 -A /usr/share/locale/locale.alias pt_BR.UTF-8
+#sudo update-locale LC_ALL=pt_BR.UTF-8 LANG=pt_BR.UTF-8
 
 #
 #https://stackoverflow.com/questions/39760663/docker-ubuntu-bin-sh-1-locale-gen-not-found
